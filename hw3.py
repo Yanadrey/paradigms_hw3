b3e295c7d8278ae013ebc0ad40b6d22f12d29fb6
Yanadrey <123734623+Yanadrey@users.noreply.github.com>
2024-01-28 23:35:46 +0300

    Add files via upload

---
+++ hw3.py
@@ -0,0 +1,69 @@
+from colorama import init
+from colorama import Fore
+
+init()
+
+board = list(range(1, 10))
+
+
+def draw_board(board):
+    print("-" * 13)
+    for i in range(3):
+        print("|", board[0 + i * 3], "|", board[1 + i * 3], "|", board[2 + i * 3], "|")
+        print("-" * 13)
+
+
+def take_input(player_input):
+    valid = False
+    while not valid:
+        player_answer = input("Куда ставить " + player_input + "? ")
+        try:
+            player_answer = int(player_answer)
+        except ValueError:
+            print("Некорректный ввод. Введите число?")
+            continue
+        if 1 <= player_answer <= 9:
+            if str(board[player_answer - 1]) not in "XO":
+                board[player_answer - 1] = player_input
+                valid = True
+            else:
+                print("Эта клетка уже занята!")
+        else:
+            print("Некорректный ввод. Введите число от 1 до 9.")
+
+
+def check_win(board):
+    win_coord = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
+    for each in win_coord:
+        if board[each[0]] == board[each[1]] == board[each[2]]:
+            return board[each[0]]
+    return False
+
+
+def main(board):
+    counter = 0
+    win = False
+    while not win:
+        draw_board(board)
+        if counter % 2 == 0:
+            print(Fore.RED)
+            take_input("X")
+        else:
+            print(Fore.GREEN)
+            take_input("O")
+        counter += 1
+
+        tmp = check_win(board)
+        if tmp:
+            print(tmp, "- ты победитель!!!")
+            win = True
+            break
+        if counter == 9:
+            print("Ничья!")
+            break
+    draw_board(board)
+
+
+main(board)
+
+input("Press enter to stop!")
